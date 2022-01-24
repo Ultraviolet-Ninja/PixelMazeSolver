12328eb1bfbc4af8325705ebe978d54142b71f52
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2022-01-24 14:19:18 -0600

    UI updates

--- src/main/java/jasmine/jragon/Controller.java
+++ src/main/java/jasmine/jragon/Controller.java
@@ -1,19 +1,55 @@
 package jasmine.jragon;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Controller {
+    @FXML
+    private ColorPicker start, finish;
+
+    @FXML
+    private MFXButton submitButton;
+
     @FXML
     private ImageView originalMazeView, solveView;
 
     @FXML
     void loadImage() {
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Image Selector");
+        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter(
+                "Image files (*.png, *.jpg)", "*.png", "*.jpg")
+        );
+
+        File file = chooser.showOpenDialog(new Stage());
+        submitButton.setDisable(file == null);
+        if (file == null)
+            return;
+        originalMazeView.setImage(new Image(String.valueOf(file)));
     }
 
     @FXML
     void saveImage() {
+        if (solveView.getImage() == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Save Error");
+            alert.setHeaderText("Solution was not created");
+            alert.setContentText("Load and solve a maze before saving the solution");
+            return;
+        }
 
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("Save Directory");
+        chooser.showDialog(new Stage());
     }
 
     @FXML
@@ -22,6 +58,14 @@ public class Controller {
 
     @FXML
     private void solveMaze() {
+        Image image = originalMazeView.getImage();
+        if (image == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Solve Error");
+            alert.setHeaderText("");
+            alert.setContentText("");
+            return;
+        }
 
     }
 }
--- src/main/java/jasmine/jragon/maze/MazeSolver.java
+++ src/main/java/jasmine/jragon/maze/MazeSolver.java
@@ -1,4 +1,9 @@
 package jasmine.jragon.maze;
 
+import javafx.scene.image.Image;
+
 public class MazeSolver {
+    public static Image solveMaze(Image original, Algorithm algorithm) {
+        return null;
+    }
 }
