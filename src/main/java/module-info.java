b982e33bf64e5fc681e773108a4eff9592c6a245
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2022-01-23 18:41:26 -0600

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module PixelMazeSolver {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.jgrapht.core;
+    requires javatuples;
+    requires MaterialFX;
+    requires org.jetbrains.annotations;
+
+    opens jasmine.jragon to javafx.fxml;
+
+    exports jasmine.jragon;
+}
